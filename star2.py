9601f5d57a1a6a4fed616f715d9d44132b764464
dudtheheaven <song9961@gmail.com>
2023-09-18 17:38:12 +0900

    V4: 오른쪽 삼각형 리팩토링

--- star2.py
+++ star2.py
@@ -1,7 +1,5 @@
-# star2.py
+# for문이용구현
 
-print(" *         ")
-print(" * *       ")
-print(" * * *     ")
-print(" * * * *   ")
-print(" * * * * * ")
+N = 6
+for i in range(1,N):
+    print(" *" * i + " "*(N-i))
